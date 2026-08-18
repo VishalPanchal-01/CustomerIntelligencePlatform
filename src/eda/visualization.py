7a682939dad358d69711191d22444e04f897f24e
VishalPanchal-01 <panchalvish001@gmail.com>
2026-08-18 22:22:02 +0530

    performing some visualization

---
+++ src/eda/visualization.py
@@ -0,0 +1,211 @@
+import os
+import sys
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from src.config.config import ProjectConfig
+from src.utils.logger import logger
+from src.utils.exception import CustomException
+
+class EDAVisualization:
+    def __init__(self):
+        self.figure_dir = os.path.join(os.getcwd(),'artifacts','eda')
+        os.makedirs(self.figure_dir , exist_ok=True)
+
+    def plot_monthly_revenue(self,monthly_revenue:pd.Series):
+        try:
+            logger.info("calculating monthly revenue.")
+            plt.figure(figsize=(12,6))
+
+            monthly_revenue.plot(kind='line',marker='o')
+            plt.title("Monthly Revenue")
+            plt.xlabel('Month')
+            plt.ylabel('Revenue')
+            plt.xticks(rotation=45)
+            plt.tight_layout()
+
+            file_path = os.path.join(self.figure_dir,'monthly_rebenue.png')
+            plt.savefig(file_path)
+            plt.close()
+            logger.info(f"Monthly Revenue plot saved : {file_path}")
+            return file_path
+            
+        except Exception as e:
+            logger.error("calculating monthly revenue failed.")
+            raise CustomException(e,sys) 
+
+    def plot_monthly_orders(self,monthly_orders: pd.Series):
+        try:
+            logger.info("Creating monthly orders plot.")
+            plt.figure(figsize=(12, 6))
+            monthly_orders.plot(kind="line",marker="o")
+            plt.title("Monthly Orders")
+            plt.xlabel("Month")
+            plt.ylabel("Number of Orders")
+            plt.xticks(rotation=45)
+            plt.tight_layout()
+            file_path = os.path.join(self.figure_dir,"monthly_orders.png")
+            plt.savefig(file_path)
+            plt.close()
+
+            logger.info(f"Monthly orders plot saved: "f"{file_path}")
+            return file_path
+
+        except Exception as e:
+            logger.error("Monthly orders plot failed.")
+            raise CustomException(e,sys)       
+
+
+    def plot_monthly_customers(self,monthly_customers: pd.Series):
+        try:
+            logger.info("Creating monthly customers plot.")
+            plt.figure(figsize=(12, 6))
+            monthly_customers.plot(kind="line",marker="o")
+            plt.title("Monthly Active Customers")
+            plt.xlabel("Month")
+            plt.ylabel("Number of Customers")
+            plt.xticks(rotation=45)
+            plt.tight_layout()
+            file_path = os.path.join(
+            self.figure_dir,"monthly_customers.png")
+            plt.savefig(file_path)
+            plt.close()
+
+            logger.info(f"Monthly customers plot saved: "f"{file_path}")
+            return file_path
+
+        except Exception as e:
+            logger.error("Monthly customers plot failed.")
+            raise CustomException(e,sys)
+        
+    def calculate_monthly_orders(self,monthly_orders:pd.Series):
+        try:
+            logger.info("calculating monthly order plot.")
+            plt.figure(figsize=(12,6))
+            monthly_orders.plot(kind='line',marker='o')
+            plt.title("Monthle orders")
+            plt.xlabel('Month')
+            plt.ylabel('No. of orders')
+            plt.xticks(rotation=45)
+            plt.tight_layout()
+            file_path = os.path.join(self.figure_dir,"monthly_orders.png")
+            plt.savefig(file_path)
+            plt.close()
+            logger.info(f"Monthly order plot saved : {file_path}")
+
+            return file_path
+        
+        except Exception as e:
+            logger.error("Monthly order plot failed.")
+            raise CustomException(e,sys)   
+
+    def calculate_monthly_customers(self,monthly_customers:pd.Series):
+        try:
+            logger.info("calculating monthly customer plot.")
+            plt.figure(figsize=(12,6))
+            monthly_customers.plot(kind='line',marker='o')
+            plt.title("Monthly Active customers")
+            plt.xlabel("Month")
+            plt.ylabel("No. of customers")
+            plt.xticks(rotation=45)
+            plt.tight_layout()
+            file_path = os.path.join(self.figure_dir,"monthly_customers.png")
+            plt.savefig(file_path)
+            plt.close()
+            logger.info(f"Monthly customer plot saved : {file_path}")
+
+            return file_path
+        
+        except Exception as e:
+            logger.error("Monthly customer plot failed.")
+            raise CustomException(e,sys)    
+
+    def plot_revenue_distribution(self,df:pd.DataFrame):
+        try:
+            logger.info("Plotting revenue distribution.")
+            plt.figure(figsize=(10,6))
+            df['Revenue'].hist(bins=50)
+            plt.title('Revenue Distribution')
+            plt.xlabel('Revenue')
+            plt.ylabel('Frequency')
+            plt.tight_layout()
+
+            file_path = os.path.join(self.figure_dir,"revenue_distribution.png")
+            plt.savefig(file_path)
+            plt.close()
+
+            logger.info(f"Revenue distribution plot saved : {file_path}")
+            return file_path
+
+        except Exception as e:
+            logger.error("Revenue distribution plot failed.")
+            raise CustomException(e,sys)    
+
+    def plot_customer_spending_distribution(self,df:pd.DataFrame):
+        try:
+            logger.info("plotting customer spending distribution.")
+            customer_spending = (df.dropna(subset=['Customer ID']).groupby('Customer ID')['Revenue'].sum())
+            plt.figure(figsize=(10,6))
+            customer_spending.hist(bins=50)
+            plt.title("Customer Spending Distribution")
+            plt.xlabel("Total Customer Spending")
+            plt.ylabel("Number of customer")
+            plt.tight_layout()
+
+            file_path = os.path.join(self.figure_dir,"customer_spending_distribution.png")
+            plt.savefig(file_path)
+            plt.close()
+            logger.info(f"Customer spending distribution plot saved : {file_path}")
+
+            return file_path
+        
+        except Exception as e:
+            logger.error("customer spending failed.")
+            raise CustomException(e,sys)    
+
+    def plot_top_countries_by_revenue(self,df:pd.DataFrame,top_n : int =10):
+        try:
+            logger.info("plotting top countries by revenue.")
+            country_revenue = (df.groupby('Country')['Revenue'].sum().sort_values(ascending=False).head(top_n))
+            plt.figure(figsize=(10,6))
+            country_revenue.sort_values().plot(kind="bar")
+            plt.title(f"Top {top_n} countries by revenue")
+            plt.xlabel("Revenue")
+            plt.ylabel("Country")
+            plt.tight_layout()
+
+            file_path = os.path.join(self.figure_dir,"top_countries_by_revenue.png")
+            plt.savefig(file_path)
+            plt.close()
+            logger.info(f"Top countries plot saved : {file_path}")
+
+            return file_path
+
+        except Exception as e:
+            logger.error("Top countries plot failed.")
+            raise CustomException(e,sys)    
+
+    def plot_top_products_by_revenue(self,df: pd.DataFrame,top_n: int = 10):
+        try:
+            logger.info("Creating top products by revenue plot.")
+            product_revenue = (df.groupby("StockCode")["Revenue"].sum().sort_values(ascending=False).head(top_n))
+            plt.figure(figsize=(10, 6))
+            product_revenue.sort_values().plot(kind="barh")
+            plt.title(f"Top {top_n} Products by Revenue")
+            plt.xlabel("Revenue")
+            plt.ylabel("Stock Code")
+            plt.tight_layout()
+
+            file_path = os.path.join(self.figure_dir,"top_products_by_revenue.png")
+            plt.savefig(file_path)
+            plt.close()
+            logger.info(f"Top products plot saved: "f"{file_path}")
+
+            return file_path
+
+        except Exception as e:
+            logger.error("Top products plot failed.")
+            raise CustomException(e,sys)
+            
---
+++ tests/test_visualization.py
@@ -0,0 +1,132 @@
+import os
+import pandas as pd
+from src.eda.visualization import EDAVisualization
+
+def test_plot_monthly_revenue():
+    visualization = EDAVisualization()
+    monthly_revenue = pd.Series(
+        [1000, 1500, 2000],
+        index=pd.to_datetime(
+            [
+                "2010-01-31",
+                "2010-02-28",
+                "2010-03-31"
+            ]
+        )
+    )
+
+    file_path = visualization.plot_monthly_revenue( monthly_revenue)
+
+    assert file_path is not None
+    assert os.path.exists(file_path)
+
+def test_plot_monthly_orders():
+    visualization = EDAVisualization()
+    monthly_orders = pd.Series(
+        [100, 150, 200],
+        index=pd.to_datetime(
+            [
+                "2010-01-31",
+                "2010-02-28",
+                "2010-03-31"
+            ]
+        )
+    )
+
+    file_path = visualization.plot_monthly_orders(monthly_orders)
+
+    assert file_path is not None
+    assert os.path.exists(file_path)
+
+def test_plot_monthly_customers():
+    visualization = EDAVisualization()
+    monthly_customers = pd.Series(
+        [50, 75, 100],
+        index=pd.to_datetime(
+            [
+                "2010-01-31",
+                "2010-02-28",
+                "2010-03-31"
+            ]
+        )
+    )
+
+    file_path = visualization.plot_monthly_customers(monthly_customers)
+
+    assert file_path is not None
+    assert os.path.exists(file_path)
+
+def test_plot_revenue_distribution():
+    visualization = EDAVisualization()
+    df = pd.DataFrame({"Revenue": [10,20,30,40,50,60,70,80,90,100]})
+    file_path = visualization.plot_revenue_distribution(df)
+
+    assert file_path is not None
+    assert os.path.exists(file_path)
+
+def test_plot_customer_spending_distribution():
+    visualization = EDAVisualization()
+    df = pd.DataFrame({"Customer ID": [101,101,102,102,103,103],"Revenue": [100,200,150,250,300,100]})
+    file_path = (visualization.plot_customer_spending_distribution(df))
+
+    assert file_path is not None
+    assert os.path.exists(file_path)
+
+
+def test_plot_top_countries_by_revenue():
+    visualization = EDAVisualization()
+
+    df = pd.DataFrame({
+        "Country": [
+            "United Kingdom",
+            "United Kingdom",
+            "Germany",
+            "France",
+            "Germany"
+        ],
+        "Revenue": [
+            1000,
+            500,
+            800,
+            600,
+            200
+        ]
+    })
+
+    file_path = (
+        visualization
+        .plot_top_countries_by_revenue(df)
+    )
+
+    assert file_path is not None
+    assert os.path.exists(file_path)
+
+
+def test_plot_top_products_by_revenue():
+
+    visualization = EDAVisualization()
+
+    df = pd.DataFrame({
+        "StockCode": [
+            "A001",
+            "A001",
+            "A002",
+            "A003",
+            "A002"
+        ],
+        "Revenue": [
+            1000,
+            500,
+            800,
+            600,
+            200
+        ]
+    })
+
+    file_path = (
+        visualization
+        .plot_top_products_by_revenue(df)
+    )
+
+    assert file_path is not None
+    assert os.path.exists(file_path)
